4db4a2f9f1c6ffc415aeecb54afbb55f3f656d91
Lana Alekseeva <107322490+schoegar@users.noreply.github.com>
2023-10-21 11:16:12 +0100

    Add IBAN countries

--- config.py
+++ config.py
@@ -7,9 +7,10 @@ bot = TeleBot(token, parse_mode='html')
 messages = {}
 
 # Main keyboard objects
-requests = ['Users', 'File', 'Credit card', 'Text', '💬 Share feedback']
+requests = ['Users', 'File', 'Credit card', 'IBAN', 'Text', '💬 Share feedback']
 users = ['1️⃣', '3️⃣', '5️⃣', '🔟']
 cards = ['MasterCard', 'VISA', 'AmEx', 'Maestro', 'Discover', 'JCB']
+iban_countries = ['GB 🇬🇧', 'BE 🇧🇪', 'GE 🇩🇪', 'FR 🇫🇷', 'NL 🇳🇱', 'PT 🇵🇹']
 formats = ['.jpg', '.png', '.svg', '.gif', '.ico', '.mp4', '.avi',
            '.webm', '.doc', '.docx', '.xls', '.xlsx', '.txt',
            '.pdf', '.css', '.html', '.js', '.json', '.zip', '.rar']
