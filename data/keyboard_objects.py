9cdfcbc4e960dcbc8001994ab1a0835869f29d8f
Lana Alekseeva <107322490+schoegar@users.noreply.github.com>
2023-11-13 00:58:00 +0000

    Move data to 'data' directory

---
+++ data/keyboard_objects.py
@@ -0,0 +1,8 @@
+# Main keyboard objects
+requests = ['Users', 'File', 'Credit card', 'IBAN', 'Text', '💬 Share feedback']
+users = ['1️⃣', '3️⃣', '5️⃣', '🔟']
+cards = ['MasterCard', 'VISA', 'AmEx', 'Maestro', 'Discover', 'JCB']
+iban_countries = ['🇬🇧 GB', '🇧🇪 BE', '🇩🇪 DE', '🇫🇷 FR', '🇳🇱 NL', '🇪🇸 ES']
+formats = ['.jpg', '.png', '.svg', '.gif', '.ico', '.mp4', '.avi',
+           '.webm', '.doc', '.docx', '.xls', '.xlsx', '.txt', '.pdf',
+           '.css', '.html', '.js', '.json', '.zip', '.rar']
