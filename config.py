2111f1863de8f2c0d3a93c511983db0841132383
Lana Alekseeva <107322490+schoegar@users.noreply.github.com>
2023-11-15 23:50:21 +0000

    Update config file

--- config.py
+++ config.py
@@ -1,16 +1,14 @@
+"""
+config.py - Configuration file for the Telegram bot.
+
+token: Telegram bot API token.
+faker: Faker instance for generating fake data.
+bot: TeleBot instance for interacting with the Telegram API.
+"""
+
 from telebot import TeleBot
 from faker import Faker
 
 token = 'TOKEN'
 faker = Faker()
 bot = TeleBot(token, parse_mode='html')
-
-commands = [
-    ("/start", "Start command"),
-    ("/users", "Generate user data"),
-    ("/file", "Generate files"),
-    ("/card", "Generate bank cards"),
-    ("/iban", "Generate IBANs"),
-    ("/text", "Generate text"),
-    ("/feedback", "Leave feedback")
-]
