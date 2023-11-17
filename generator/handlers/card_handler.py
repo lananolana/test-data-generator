0bbab7f83086bd7edaa86d64c95f05f21ed66f22
Lana Alekseeva <107322490+schoegar@users.noreply.github.com>
2023-11-17 22:35:52 +0000

    Minor fix (PEP 8)

--- generator/handlers/card_handler.py
+++ generator/handlers/card_handler.py
@@ -1,13 +1,13 @@
 from telebot import types
 from config import faker, bot, cards_objects
-from utils.common_handlers import Handlers
+from utils.common_handlers import handlers
 from utils.constants import common, accounts
 
 
 @bot.message_handler(commands=['card'])
 def card_handler(message):
-    card_markup = Handlers.markup_setup(*cards_objects.cards, 3)
-    Handlers.send_message(
+    card_markup = handlers.markup_setup(*cards_objects.cards, 3)
+    handlers.send_message(
         message, accounts.CARDS_GENERATOR, card_markup, payment_system
     )
 
@@ -17,13 +17,13 @@ def payment_system(message: types.Message):
 
     if card:
         card_data = faker.credit_card_full(card)
-    elif Handlers.is_back_to_start(message.text):
-        Handlers.welcome(message)
+    elif handlers.is_back_to_start(message.text):
+        handlers.welcome(message)
     else:
-        Handlers.error(message, payment_system)
+        handlers.error(message, payment_system)
 
     reply = f"{message.text} card details:\n\n<code>{card_data}</code>"
-    Handlers.send_message(message, reply)
-    Handlers.send_message(
+    handlers.send_message(message, reply)
+    handlers.send_message(
         message, common.GENERATOR_AGAIN, next_handler=payment_system
     )
