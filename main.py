4a14ac2b4894e2a0f0c5e80b0010ff129785b589
Lana Alekseeva <107322490+schoegar@users.noreply.github.com>
2023-11-18 23:20:14 +0000

    Import fix

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ main.py - Main script to run the Telegram bot.
 test_bot: An instance of the bot used for testing purposes.
 """
 
-from generator import bot
+from generator.bot import Generator
 
 if __name__ == '__main__':
-    test_bot = bot()
+    test_bot = Generator()
     test_bot.run()
