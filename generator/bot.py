c6dd6846cc771d989ce7d63cae8faf93116235a8
Lana Alekseeva <107322490+schoegar@users.noreply.github.com>
2023-11-21 23:40:48 +0000

    Move welcome handler

--- generator/bot.py
+++ generator/bot.py
@@ -1,17 +1,10 @@
 import json
 from config import bot
 from telebot.types import BotCommand
-from data.commands import COMMANDS
-from utils.bot_handler import BotHandler
-from utils.constants import common
 
 
 class Generator:
     def __init__(self):
-        self.command_handlers = {
-            command.command: BotHandler.get_handler(command.handler)
-            for command in COMMANDS
-        }
         self.load_messages()
 
     def load_messages(self):
@@ -27,14 +20,3 @@ class Generator:
     def run(self):
         bot.set_my_commands(self.bot_commands)
         bot.infinity_polling()
-
-    @bot.message_handler(commands=['start'])
-    def welcome(self, message):
-        command = message.text
-        if command in self.command_handlers:
-            BotHandler.send_welcome_message(message)
-            self.command_handlers[command](message)
-        else:
-            BotHandler.error(
-                message, common.QUERY_ERROR, next_handler=self.welcome
-            )
