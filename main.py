1de9af2eff00cedc80d1c8641232e045da2fe518
Lana Alekseeva <107322490+schoegar@users.noreply.github.com>
2023-11-22 23:44:44 +0000

    Change variable name

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 """
 main.py - Main script to run the Telegram bot.
-
-test_bot: An instance of the bot used for testing purposes.
 """
 
 from generator.bot import Generator
 
 if __name__ == '__main__':
-    test_bot = Generator()
-    test_bot.run()
+    bot = Generator()
+    bot.run()
